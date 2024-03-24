a7f73417f05dec2de069e60251d1793130176a46
dark-doraemon <tuandinhbi75@gmail.com>
2024-03-24 18:49:25 +0700

    error service, need to fix

--- services-start/src/app/app.module.ts
+++ services-start/src/app/app.module.ts
@@ -6,12 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { LoggingService } from './services/logging.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountComponent,
-    NewAccountComponent
+    NewAccountComponent,
   ],
   imports: [
     BrowserModule,
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../services/logging.service';
+import { AccountService } from '../services/account.service';
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+    selector: 'app-new-account',
+    templateUrl: './new-account.component.html',
+    styleUrls: ['./new-account.component.css'],
+    providers: [LoggingService,AccountService]//nó sẽ thông báo cho angular biết rằng component này cần được inject các dịch vụ này
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+    @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
-  }
+    constructor(private loggingService: LoggingService,
+                private accountService : AccountService) { }
+
+    onCreateAccount(accountName: string, accountStatus: string) {
+        this.accountService.onAccountAdded({name : accountName,status : accountStatus});
+        
+    }
 }
